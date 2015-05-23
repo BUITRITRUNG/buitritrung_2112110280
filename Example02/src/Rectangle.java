f2b6ce077f21d9855fd66ce67dbe687cafac925d
Admin <Admin@D20903>
2015-05-23 14:32:19 +0700

    trung

---
+++ Example02/src/Rectangle.java
@@ -0,0 +1,20 @@
+
+public class Rectangle {
+	int width ;
+	int height;
+	public Rectangle(int a,int b){
+		width=a;
+		height=b;
+	}
+public int area(){
+	return (width*height);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle recta=new Rectangle(5,4);
+Rectangle rectb=new Rectangle(5,6);
+System.out.println("rect a:" +recta.area());
+System.out.println("rect b:" +rectb.area());
+	}
+
+}
