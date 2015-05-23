93f88c6e70b9bd91d4d040e620f0946b36f90d05
Admin <Admin@D20903>
2015-05-23 14:37:08 +0700

    trung

---
+++ Example05/src/Program.java
@@ -0,0 +1,14 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle rect =new Rectangle();
+Triangle  trgl=new Triangle();
+rect.setValues(4, 5);
+trgl.setValues(4, 5);
+System.out.println("rect :"+rect.area());
+System.out.println("trgl :"+trgl.area());
+	}
+
+}
